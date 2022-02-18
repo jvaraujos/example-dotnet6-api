5414a3c4f403d00c8936f12e5e3683fd231009f6
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-18 13:19:43 -0300

    feat: include exceptions for middleware

---
+++ Example.Application/Exceptions/IdentityException.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Example.Application.Exceptions
+{
+    public class IdentityException : ApplicationException
+    {
+        public List<string> IdentityErrors { get; set; }
+
+        public IdentityException(IEnumerable<IdentityError> identityErrors)
+        {
+            IdentityErrors = new List<string>();
+
+            foreach (var validationError in identityErrors)
+                IdentityErrors.Add(validationError.Description);
+        }
+    }
+}
