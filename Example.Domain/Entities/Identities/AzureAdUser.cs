78fdd0caf49b1d35926841f7a7e27947ee30233e
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-18 13:18:18 -0300

    feat: include entities on domain

---
+++ Example.Domain/Entities/Identities/AzureAdUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example.Domain.Entities.Identities
+{
+    public class AzureAdUser
+    {
+        public string DisplayName { get; set; }
+        public string GivenName { get; set; }
+        public string JobTitle { get; set; }
+        public string Mail { get; set; }
+        public string MobilePhone { get; set; }
+        public string OfficeLocation { get; set; }
+        public string PreferredLanguage { get; set; }
+        public string Surname { get; set; }
+        public string UserPrincipalName { get; set; }
+        public Guid Id { get; set; }
+    }
+}
---
+++ Example.Domain/Entities/Identities/JwtToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example.Domain.Entities.Identities
+{
+    public class JwtToken
+    {
+        public string Token { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
