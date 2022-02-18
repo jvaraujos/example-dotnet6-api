07c701a5815c29e770ccbf2496bb6313eb66952d
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-18 13:22:27 -0300

    feat: include identityDbContext

---
+++ Example.Identity/ExampleIdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Example.Domain.Entities.Identities;
+
+namespace Example.Identity
+{
+    public class ExampleIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ExampleIdentityDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
