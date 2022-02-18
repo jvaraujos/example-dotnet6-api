5614d8fcbc92e785032aac067451dc0817badd80
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-18 13:21:56 -0300

    feat: include firstUser

---
+++ Example.Identity/Seed/CreateFirstUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Example.Identity.Seed
+{
+    public static class UserCreator
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
+        {
+            var applicationUser = new ApplicationUser
+            {
+                FirstName = "Maria Joaquina",
+                LastName = "Amaral Pereira Gloria",
+                UserName = "userTest",
+                Email = "test@hotmail.com",
+                EmailConfirmed = true
+            };
+
+            var user = await userManager.FindByEmailAsync(applicationUser.Email);
+            if (user == null)
+            {
+                await userManager.CreateAsync(applicationUser, "BancoDev@123");
+            }
+        }
+    }
+}
