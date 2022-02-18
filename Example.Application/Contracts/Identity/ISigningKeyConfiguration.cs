6f899826e84dfe3430411e876716cc7e4e7ea940
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-18 13:19:26 -0300

    feat: include contracts for identities

---
+++ Example.Application/Contracts/Identity/ISigningKeyConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example.Application.Contracts.Identity
+{
+    public interface ISigningKeyConfiguration
+    {
+        SigningCredentials SigningCredentials { get; }
+        SecurityKey SecurityKey { get; }
+    }
+}
