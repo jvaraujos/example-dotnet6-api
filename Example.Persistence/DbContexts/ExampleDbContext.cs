ec2ba99b1cd6cb1f0ae9205cdbcc09ef6430a0cb
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-18 13:23:13 -0300

    feat: inlcude dbcontext for persistence

---
+++ Example.Persistence/DbContexts/ExampleDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Audit.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Example.Domain.Entities.Logs;
+using System.Reflection;
+
+namespace Example.Persistence.DbContexts
+{
+    public class ExampleDbContext : AuditDbContext
+    {
+        public ExampleDbContext(DbContextOptions<ExampleDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<LogErro> LogErros { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //Remove delete cascade
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+    }
+}
