a8560928f9cc45d2eaa486192a1eed8c2baf2593
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-18 13:20:20 -0300

    include: configuration in identity

---
+++ Example.Identity/Configuration/RefreshTokenConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Example.Domain.Entities.Logs;
+
+namespace Example.Identity.Configuration
+{
+    //public class RefreshTokenConfiguration : IEntityTypeConfiguration<LogErro>
+    //{
+    //    public void Configure(EntityTypeBuilder<LogErro> builder)
+    //    {
+    //        builder.HasKey(p => p.Id);
+    //    }
+    //}
+}
---
+++ Example.Identity/Configuration/SigningKeyConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Example.Application.Contracts.Identity;
+using System.Text;
+
+namespace Example.Identity.Configuration
+{
+    public class SigningKeyConfiguration : ISigningKeyConfiguration
+    {
+        public SigningCredentials SigningCredentials { get; }
+        public SecurityKey SecurityKey { get; }
+
+        public SigningKeyConfiguration(string secretKey)
+        {
+            byte[] key = Encoding.UTF8.GetBytes(secretKey);
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+
+            SecurityKey = securityKey;
+
+            SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+        }
+    }
+}
